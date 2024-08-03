d2c29b236d975040380d2d662d22bbd0f822969c
AbRahmanMamnoon <abdulrahmanmamnoon82!@gmail.com>
2024-08-03 16:24:04 +0430

    Password encrypting and hashing logic moved from userController to userModel

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,22 +1,19 @@
-import User from "../models/userModel.js";
-import bcrypt from "bcryptjs";
+import User from '../models/userModel.js';
 
-import asyncHandler from "../middlewares/asyncHandler.js";
-import generateToken from "../utils/create-token.js";
+import asyncHandler from '../middlewares/asyncHandler.js';
+import generateToken from '../utils/create-token.js';
 
 const createUser = asyncHandler(async (req, res) => {
   const { fullName, email, password, isAdmin, image } = req.body;
 
   if ((!fullName || !email, !password)) {
-    throw new Error("Please fill all the fields");
+    throw new Error('Please fill all the fields');
   }
 
   const userExist = await User.findOne({ email });
-  if (userExist) throw new Error("User already exist");
+  if (userExist) throw new Error('User already exist');
 
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(password, salt);
-  const currentUser = new User({ fullName, email, password: hashedPassword });
+  const currentUser = new User({ fullName, email, password, image });
   try {
     await currentUser.save();
 
@@ -30,46 +27,39 @@ const createUser = asyncHandler(async (req, res) => {
     });
   } catch (error) {
     res.status(400);
-    throw new Error("Invalid user data");
+    throw new Error('Invalid user data');
   }
 });
 
 const loginUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
   if ((!email, !password)) {
-    throw new Error("Please fill all the fields");
+    throw new Error('Please fill all the fields');
   }
 
   const currentUser = await User.findOne({ email });
 
-  if (currentUser) {
-    const isPasswordValid = await bcrypt.compare(
-      password,
-      currentUser.password
-    );
-
-    if (isPasswordValid) {
-      generateToken(res, currentUser._id);
-      res.status(200).json({
-        _id: currentUser._id,
-        fullName: currentUser.fullName,
-        email: currentUser.email,
-        isAdmin: currentUser.isAdmin,
-      });
-    }
+  if (currentUser && (await currentUser.isPasswordValid(password))) {
+    generateToken(res, currentUser._id);
+    res.status(200).json({
+      _id: currentUser._id,
+      fullName: currentUser.fullName,
+      email: currentUser.email,
+      isAdmin: currentUser.isAdmin,
+    });
   } else {
     res.status(401);
-    throw new Error("You are not logged in pleas log in!");
+    throw new Error('You are not logged in pleas log in!');
   }
 });
 
 const logoutCurrentUser = (req, res) => {
-  res.cookie("jwt", " ", {
+  res.cookie('jwt', ' ', {
     httpOnly: true,
     expires: new Date(0),
   });
 
-  res.status(200).json({ message: "Logout successfully" });
+  res.status(200).json({ message: 'Logout successfully' });
 };
 
 const getAllUsers = asyncHandler(async (req, res) => {
@@ -92,7 +82,7 @@ const getCurrentUserProfile = asyncHandler(async (req, res) => {
     });
   } else {
     res.status(404);
-    throw new Error("User not found!");
+    throw new Error('User not found!');
   }
 });
 
@@ -103,11 +93,8 @@ const updateCurrentUserProfile = asyncHandler(async (req, res) => {
   if (currentUser) {
     currentUser.fullName = fullName || currentUser.fullName;
     currentUser.email = email || currentUser.email;
-
     if (password) {
-      const salt = await bcrypt.genSalt(10);
-      const hashedPassword = await bcrypt.hash(password, salt);
-      currentUser.password = hashedPassword;
+      currentUser.password = password;
     }
 
     await currentUser.save();
@@ -126,7 +113,7 @@ const findUserByID = asyncHandler(async (req, res) => {
     res.status(200).json(user);
   } else {
     res.status(404);
-    throw new Error("USer not found!");
+    throw new Error('USer not found!');
   }
 });
 
@@ -148,7 +135,7 @@ const updateUserById = asyncHandler(async (req, res) => {
     });
   } else {
     res.status(404);
-    throw new Error("User not found!");
+    throw new Error('User not found!');
   }
 });
 
@@ -157,13 +144,13 @@ const deleteUserByID = asyncHandler(async (req, res) => {
   if (user) {
     if (user.isAdmin) {
       res.status(400);
-      throw new Error("Can not delete user as admin!");
+      throw new Error('Can not delete user as admin!');
     }
 
     await User.deleteOne({ _id: user._id });
-    res.status(204).json({ message: "User removed successfully" });
+    res.status(204).json({ message: 'User removed successfully' });
   } else {
-    res.status(404).json({ message: "User not found!" });
+    res.status(404).json({ message: 'User not found!' });
   }
 });
 
