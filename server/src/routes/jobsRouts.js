6ce80c8a7ad0a2c3afee431828bd9326f18d712b
AbRahmanMamnoon <abdulrahmanmamnoon82!@gmail.com>
2024-08-05 10:48:49 +0430

    createJob endpoint added

--- server/src/routes/jobsRouts.js
+++ server/src/routes/jobsRouts.js
@@ -1,11 +1,10 @@
-const express = require('express');
-const jobController = require('../controllers/jobController');
+import express from 'express';
+import { createJob } from '../controllers/jobController.js';
 const router = express.Router();
 
-router.route('/').get(jobController.findJobs);
-router.route('/industries').get(jobController.getIndustries);
-router.route('/companys').get(jobController.getCompanys);
-router.route('/locations').get(jobController.getLocations);
+router.route('/').post(createJob);
+// router.route('/industries').get(jobController.getIndustries);
+// router.route('/companys').get(jobController.getCompanys);
+// router.route('/locations').get(jobController.getLocations);
 
-
-module.exports = router;
+export default router;
