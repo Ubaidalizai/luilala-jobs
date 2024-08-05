5eb0581a145e54895b0860c889bb22b3b728d2f8
fazalullahibrahimi <fazalullahrasoli10@gmail.com>
2024-08-05 03:06:22 -0700

    i changed jobs route

--- server/src/routes/jobsRouts.js
+++ server/src/routes/jobsRouts.js
@@ -1,10 +1,17 @@
 import express from 'express';
-import { createJob } from '../controllers/jobController.js';
+import {
+  createJob,
+  findJobs,
+  getAllJobs,
+  getCompanys,
+  getIndustries,
+  getLocations,
+} from '../controllers/jobController.js';
 const router = express.Router();
 
-router.route('/').post(createJob);
-// router.route('/industries').get(jobController.getIndustries);
-// router.route('/companys').get(jobController.getCompanys);
-// router.route('/locations').get(jobController.getLocations);
+router.route('/').post(createJob).get(getAllJobs);
+router.route('/industry').get(getIndustries);
+router.route('/company').get(getCompanys);
+router.route('/location').get(getLocations);
 
 export default router;
