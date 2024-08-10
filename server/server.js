62edc825d91c5e69f432112c7072038d897131ee
fazalullahibrahimi <fazalullahrasoli10@gmail.com>
2024-08-10 05:32:55 -0700

    I added jobAlert route

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ import userRouts from './src/routes/userRouts.js';
 import courseRoute from './src/routes/coursRoute.js';
 import employerRoute from './src/routes/EmployerRoute.js';
 import jobRoute from './src/routes/jobsRouts.js';
+import jobAlertRoute from './src/routes/jobAlertRoute.js';
 
 // utils
 import connectDB from './src/config/db.js';
@@ -34,6 +35,7 @@ app.use('/api/v1/users', userRouts);
 app.use('/api/v1/cours', courseRoute);
 app.use('/api/v1/employer', employerRoute);
 app.use('/api/v1/job', jobRoute);
+app.use('/api/v1/jobAlert', jobAlertRoute);
 
 const port = process.env.PORT || 3000;
 app.listen(port, console.log(`server is running on port ${port}...`));
