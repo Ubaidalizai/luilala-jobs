9f9304fa4cb18056b839a939aed7ab1544f33746
AbRahmanMamnoon <abdulrahmanmamnoon82!@gmail.com>
2024-09-11 15:59:19 +0430

    Added: route of get all job of current employer

--- server/src/routes/EmployerRoute.js
+++ server/src/routes/EmployerRoute.js
@@ -15,6 +15,7 @@ import {
   updateEmployerPhoto,
   updatePassword,
   employersLogos,
+  getAllJobsOfCurrentEmployer,
 } from '../controllers/EmployerController.js';
 
 import { authenticate, authorizeAdmin } from '../middlewares/authMiddleware.js';
@@ -28,6 +29,7 @@ router.route('/logout').post(logoutCurrentEmployer);
 router.route('/search').get(authenticate, searchEmployerByName);
 router.route('/update-password').patch(authenticate, updatePassword);
 router.route('/logos').get(employersLogos);
+router.route('/jobs').get(authenticate, getAllJobsOfCurrentEmployer);
 
 router.patch(
   '/updateMe',
